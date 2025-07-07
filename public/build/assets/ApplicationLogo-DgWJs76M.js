7c1d24287b188e512c66e92e7387ffd5732c582e
pisey1993 <pisey@peoplenpartners.com>
2025-07-07 17:40:52 +0700

    Revert "revert"
    This reverts commit 1ad3d5ff55544241d948b07dc5f2b95b9c8868d0.

---
+++ public/build/assets/ApplicationLogo-DgWJs76M.js
@@ -0,0 +1,5 @@
+<<<<<<<< HEAD:public/build/assets/ApplicationLogo-DNhug7Wg.js
+import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as t,o as e}from"./app-CnSrZFA8.js";const c={},r={src:"https://www.peoplenpartners.com/img/logo.png",style:{width:"150px",height:"100px"}};function n(s,p){return e(),t("img",r)}const a=o(c,[["render",n]]);export{a as A};
+========
+import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as t,o as e}from"./app-BHXfOjrs.js";const c={},r={src:"https://www.peoplenpartners.com/img/logo.png",style:{width:"150px",height:"100px"}};function n(s,p){return e(),t("img",r)}const a=o(c,[["render",n]]);export{a as A};
+>>>>>>>> parent of 1ad3d5f (revert):public/build/assets/ApplicationLogo-DgWJs76M.js
